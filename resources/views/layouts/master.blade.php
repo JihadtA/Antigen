2d4b651230902248ca6a51d28caa35d643c1bf0e
Jihadt Akbar <jihadt.a56@gmail.com>
2021-10-08 13:46:33 +0700

    template_jr

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,204 @@
+<!--
+*
+*  INSPINIA - Responsive Admin Theme
+*  version 2.8
+*
+-->
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>ANTIGEN | @yield('title')</title>
+    <link href="{{ asset('/inspinia/css/all.css') }}" rel="stylesheet">
+    <link href="{{ asset('/inspinia/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/inspinia/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('/inspinia/css/plugins/dataTables/datatables.min.css') }}" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('assets/css/sweetalert2.css')}}">
+
+    <!-- Toastr style -->
+    <link href="{{ asset('/inspinia/css/plugins/toastr/toastr.min.css') }}" rel="stylesheet">
+
+    <!-- Gritter -->
+    <link href="{{ asset('/inspinia/js/plugins/gritter/jquery.gritter.css') }}" rel="stylesheet">
+    <link href="{{ asset('/inspinia/css/plugins/select2/select2.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('/inspinia/css/animate.css') }}" rel="stylesheet">
+    <link href="{{ asset('/inspinia/css/style.css') }}" rel="stylesheet">
+
+    {{-- <style>
+        body{
+            font-family: "open sans", "Helvetica Neue", Arial, Helvetica, sans-serif;
+            background-color: #275783;
+            font-size: 13px;
+            color: #676a6c;
+            overflow-x: hidden
+        }
+        .nav-header{
+            padding:33px 25px;
+            background-color: #275783;
+            background-image: none;
+        }
+        .nav > li.active{
+            border-left:4px solid #0d8f94;
+            background: #293864;
+        }
+    </style> --}}
+    <style>
+        ul.pagination{
+        display: inline-flex;
+    }
+    .select2-selection {
+        height: 34px !important;
+        border-color: #ced4da !important;
+    }
+    </style>
+
+    @stack('stylesheets')
+</head>
+
+<body>
+    <div id="wrapper">
+        @include('layouts.sidebar')
+
+    <div id="page-wrapper" class="gray-bg dashbard-1">
+        
+        <div class="row border-bottom">
+        <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+            <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+        </div>
+            <ul class="nav navbar-top-links navbar-right">
+
+                <li>
+                    <a href="#">
+                        <i class="fa fa-sign-out"></i> Log out
+                    </a>
+                </li>
+
+            </ul>
+
+        </nav>
+		</div>
+
+        
+        <div class="row wrapper border-bottom white-bg page-heading mb-3">
+		    <div class="col-lg-10">
+		        <h2>@yield('judultabel')</h2>
+		        <ol class="breadcrumb">
+		            <li class="breadcrumb-item">
+		                <a href="#">Home</a>
+		            </li>
+		            <li class="breadcrumb-item">
+		                @yield('menu1')
+		            </li>
+		            <li class="breadcrumb-item active">
+		                <strong>@yield('menu2')</strong>
+		            </li>
+		        </ol>
+		    </div>
+		    <div class="col-lg-2">
+
+		    </div>
+		</div>
+
+        @yield('content')
+
+        
+        <div class="footer">
+            <div>
+				<strong>2021</strong>© ANTIGEN Support By <a href="#" style="color: red">Our</a>
+       		</div>
+		</div>
+    </div>
+
+
+    <!-- Mainly scripts -->
+    <script src="{{ asset('/inspinia/js/jquery-3.1.1.min.js') }}"></script>
+    <script src="{{ asset('/inspinia/js/popper.min.js') }}"></script>
+    <script src="{{ asset('/inspinia/js') }}/bootstrap.js"></script>
+    <script src="{{ asset('/inspinia/js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>
+    <script src="{{ asset('/inspinia/js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+    <script src="{{ asset('assets/js/select2.full.min.js')}}"></script>
+
+    {{-- DataTables --}}
+    <script src="{{ asset('/inspinia/js/plugins/dataTables/datatables.min.js') }}"></script>
+    <script src="{{ asset('/inspinia/js/plugins/dataTables/dataTables.bootstrap4.min.js') }}"></script>
+    <script>
+        $(document).ready(function(){
+            $('.dataTables-examplee').DataTable({
+                pageLength: 25,
+                responsive: true,
+                dom: '<"html5buttons"B>lTfgitp',
+                buttons: [
+                    { extend: 'copy'},
+                    {extend: 'csv'},
+                    {extend: 'excel', title: 'ExampleFile'},
+                    {extend: 'pdf', title: 'ExampleFile'},
+
+                    {extend: 'print',
+                     customize: function (win){
+                            $(win.document.body).addClass('white-bg');
+                            $(win.document.body).css('font-size', '10px');
+
+                            $(win.document.body).find('table')
+                                    .addClass('compact')
+                                    .css('font-size', 'inherit');
+                    }
+                    }
+                ]
+
+            });
+
+        });
+
+    </script>
+
+    <!-- Flot -->
+    <script src="{{ asset('/inspinia/js/plugins/flot/jquery.flot.js') }}"></script>
+    <script src="{{ asset('/inspinia/js/plugins/flot/jquery.flot.tooltip.min.js') }}"></script>
+    <script src="{{ asset('/inspinia/js/plugins/flot/jquery.flot.spline.js') }}"></script>
+    <script src="{{ asset('/inspinia/js/plugins/flot/jquery.flot.resize.js') }}"></script>
+    <script src="{{ asset('/inspinia/js/plugins/flot/jquery.flot.pie.js') }}"></script>
+
+    <!-- Peity -->
+    <script src="{{ asset('/inspinia/js/plugins/peity/jquery.peity.min.js') }}"></script>
+    <script src="{{ asset('/inspinia/js/demo/peity-demo.js') }}"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="{{ asset('/inspinia/js/inspinia.js') }}"></script>
+    <script src="{{ asset('/inspinia/js/plugins/pace/pace.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/sweetalert2.js')}}"></script>
+    <script src="{{ asset('assets/js/jquery.validate.js')}}"></script>
+
+    <!-- jQuery UI -->
+    <script src="{{ asset('/inspinia/js/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+
+    <!-- GITTER -->
+    <script src="{{ asset('/inspinia/js/plugins/gritter/jquery.gritter.min.js') }}"></script>
+
+    <!-- Sparkline -->
+    <script src="{{ asset('/inspinia/js/plugins/sparkline/jquery.sparkline.min.js') }}"></script>
+
+    <!-- Sparkline demo data  -->
+    <script src="{{ asset('/inspinia/js/demo/sparkline-demo.js') }}"></script>
+
+    <!-- ChartJS-->
+    <script src="{{ asset('/inspinia/js/plugins/chartJs/Chart.min.js') }}"></script>
+
+    <!-- Toastr -->
+    <script src="{{ asset('/inspinia/js/plugins/toastr/toastr.min.js') }}"></script>
+
+
+
+
+@stack('scripts')
+</body>
+</html>
