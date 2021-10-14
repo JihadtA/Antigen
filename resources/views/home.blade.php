7f8088201b559bc6e7ea303811c5713c238f6bed
Jihadt Akbar <jihadt.a56@gmail.com>
2021-10-14 15:11:50 +0700

    fix-breadcrumb_jr

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,7 @@
 @extends('layouts.master')
 @section('breadcrumb')
-
+    <h2>{{ $title }}</h2>
 @endsection
 @section('content')
-<div class="row wrapper border-bottom white-bg page-heading">
-    <div class="col-lg-10">
-        <h2>{{ $title }}</h2>
-    </div>
-</div>
 <p>Halaman Home</p>
 @endsection
