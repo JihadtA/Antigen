62bf290011e869387ffd956e96567c462d6a785f
hanungstwn <hanungsetiawan23@gmail.com>
2021-10-19 15:46:21 +0700

    ubahprofileblade_hanung

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,7 +3,7 @@
     <h2>{{ $title }}</h2>
 @endsection
 @section('content')
-<p><img src="img/{{ $image }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle"></p>
+<p><img src="img/{{ $image }}" alt="{{ $name }}" width="700" height="330"></p>
 <h3>{{ $name }}</h3>
 <p>{{ $email }}</p>
 
