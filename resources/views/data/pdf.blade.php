c34afba6cc77fdc195287603ac71c133d9988895
hanungstwn <hanungsetiawan23@gmail.com>
2021-11-12 15:37:31 +0700

    fixpdf_hanung

--- resources/views/data/pdf.blade.php
+++ resources/views/data/pdf.blade.php
@@ -25,6 +25,9 @@
             padding-left: 30px;
             padding-right: 30px;
         }
+        .wrapper1{
+            padding-right: 30px;
+        }
         .col-md-{
             column-width: 30px;
             max-container-width: 720px;
@@ -46,23 +49,31 @@
         .bb-1{
             border-bottom: 1px solid black;
         }
+        .br-1{
+            border-right: 1px solid black;
+        }
+        .bl-1{
+            border-left: 1px solid black;
+        }
         .mt-1{
             margin-top: 5px;
         }
+        .mt-2{
+            margin-top: 15px;
+        }
         .mb-1{
-            margin-top: 5px;
+            margin-bottom: 5px;
         }
         .pdl{
             padding-left: 35px;
         }
+        .pdl1{
+            padding-left: 77px;
+        }
         table tr th,table tr td{
             text-align: left;
         }
-        .table-borderless{
-            
-        }
-        .spacer{
-            margin-left: 20px;
+        .table-borderless{  
         }
     </style>
 </head>
@@ -73,57 +84,60 @@
     </div>
 
     <div class="wrapper">
-        <div class="bt-1 bb-1">
+        <div class="bt-1">
             <div class="mt-1">
-                <table class="full left-container table table-borderless mt-1 mb-1">
+                <table class="full left-container table table-borderless mt-1">
                     <tbody>
                     <tr>
                         <th>No. Lab</th>
                         <td>{{ $pasien->no_lab }}</td>
-                    </tr>
-                    <tr>
-                        <th>No. Rekam Medis</th>
-                        <td>{{ $pasien->no_rm }}</td>
-                    </tr>
-                    <tr>
-                        <th>Nama</th>
-                        <td>{{ $pasien->nama }}</td>
                         <th>Nama Dokter</th>
                         <td>{{ $pasien->nama_dok }}</td>
                     </tr>
                     <tr>
-                        <th>Tanggal Lahir</th>
-                        <td>{{ $pasien->tgl_lahir }}</td>
-                        <th>No HP</th>
-                        <td>{{ $pasien->no_hp }}</td>
+                        <th>No. Rekam Medis</th>
+                        <td>{{ $pasien->no_rm }}</td>
+                        <th>Lokasi</th>
+                        <td>{{ $pasien->lokasi }}</td>
                     </tr>
                     <tr>
-                        <th>Umur</th>
-                        <td>{{ $pasien->umur }}</td>
+                        <th>Nama</th>
+                        <td>{{ $pasien->nama }}</td>
                         <th>Tanggal Transaksi</th>
                         <td>{{ $pasien->updated_at }}</td>
                     </tr>
                     <tr>
-                        <th>Alamat</th>
-                        <td>{{ $pasien->alamat }}</td>
+                        <th>Tanggal Lahir</th>
+                        <td>{{ $pasien->tgl_lahir }}</td>
                         <th>Hasil Selesai</th>
                         <td>{{ $pasien->updated_at }}</td>
                     </tr>
                     <tr>
-                        <th>Lokasi</th>
-                        <td>{{ $pasien->lokasi }}</td>
+                        <th>Umur</th>
+                        <td>{{ $pasien->umur }}</td>
                         <th>Tanggal Cetak</th>
-                        <td><?php date_default_timezone_set('Asia/Jakarta'); echo date("Y-m-d  H:i:s");?></td> 
+                        <td><?php date_default_timezone_set('Asia/Jakarta'); echo date("Y-m-d  H:i:s");?></td>
                     </tr>
-                    </tbody>
-                </table>
-            </div>
+                    <tr>
+                        <th>No HP</th>
+                        <td>{{ $pasien->no_hp }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="full wrapper1 table table-borderless mb-1">
+                <tbody>
+                    <tr>
+                        <th>Alamat</th>
+                        <td class="pdl1">{{ $pasien->alamat }}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 
-    <div class="wrapper mt-2">
-        <table class="mid table table-borderless mt-1 mb-1">
-            <thead class="bb-1">
+    <div class="wrapper1">
+        <table class="full left-container table table-borderless mt-1 mb-1">
+            <thead class="bt-1 bb-1 mb-1">
                 <tr>
                     <th scope="col">PEMERIKSAAN</th>
                     <th scope="col">METODE</th>
@@ -133,7 +147,7 @@
                 </tr>
             </thead>
 
-            <tbody>
+            <tbody class="mt-2">
                 <tr>
                     <td><i><b>IMUNOLOGI</b></i></td>
                 <tr>
@@ -163,10 +177,10 @@
         </table>
     </div>
 
-    <div class="wrapper mt-2">
-        <div class = "bb-1">
-            <div class="mt-1">
-                <table class="mid table table-borderless mt-1 mb-1" style="margin-bottom: 10px">
+    {{-- <div class="wrapper mt-2"> --}}
+        <div class = "bt-1 bb-1 br-1 bl-1">
+            {{-- <div class="mt-1"> --}}
+                <table class="full table table-borderless mt-2 mb-1" style="margin-bottom: 10px">
                     <tbody>
                         <tr>
                             <td><i><b>INTERPRETASI</b></i></td>
@@ -194,20 +208,21 @@
                         </tr>
                     </tbody>
                 </table>
-            </div>
+            {{-- </div> --}}
         </div>
-    </div>
+    {{-- </div> --}}
     
-    <div class="center mid">
+    <div class="left mid">
         <div class="mt-5">
-        <h3>Scan Me!</h3>
-        <div class="mt-5">
-        <img style="width: 3cm height: 3cm;" src="data:image/png;base64, {!! $qrcode !!}">
+        <h3 style="margin-left: 28px">Scan Me!</h3>
+        <h4 class="right" style="margin-right: 35px">Doktor Penanggung Jawab</h4>
+        <img style="width: 3cm height: 3cm; margin-left: 10px" src="data:image/png;base64, {!! $qrcode !!}">
+        <h4 class="right" style="margin-right: 74px">{{ $pasien->nama_dok }}</h4>
     </div>
     <br/>
     <br/>
     <hr/>
-    <footer>
+    <footer class="center mid">
         <p>&copy; <a href="http://127.0.0.1:8000/profile">www.antigentest.com</p>
     </footer>
 </body>
